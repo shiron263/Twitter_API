6bbaab45bb624d87d2af8c8f941dfa3d54cfc5bd
Shiron BESKIWIN <shiron.beskiwin-yestin@epitech.eu>
2023-01-06 21:12:41 +0100

    [DELETE] delete useless folder and remove auth info

--- auth.py
+++ auth.py
@@ -3,10 +3,10 @@
 
 import tweepy, datetime, sys, json, time
 
-consumerKey = '4Hs1cHal1n46fUAAt3FbtkTUh'
-consumerSecret = 'alXV7bzdigd4Y6fmB2ZQT6svWGv2QEO7Jw1GPjDHhAxD9Qy9Ik'
-accessToken = '761092062166917120-JTcJCIEBsNzAc1EGYmTD8zARkcxohOG'
-accessTokenSecret = 'lbyMWLDEbTnd0ChnetHPLK59WzqP91Nqi9RaXXTPrAO83'
+consumerKey = ''
+consumerSecret = ''
+accessToken = ''
+accessTokenSecret = ''
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 
